81222ff12c627ac0b702c9c3f3b9e856aa93a086
Charlie Rydholm <31844668+Lillmygel@users.noreply.github.com>
2018-11-08 15:00:06 +0100

    Added CustomerList to IndexView

---
+++ CharlieBankApp/Data/BankRepostitory.cs
@@ -0,0 +1,29 @@
+﻿using CharlieBankApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CharlieBankApp.Data
+{
+    public class BankRepostitory
+    {
+        public List<Customer> CustomerList { get; set; }
+
+        public BankRepostitory()
+        {
+            var listOfCustomers = new List<Customer>();
+            var listOfRandomNames = new List<string>() { "Henrik dahl", "Kapten Mygel", "Henrietta Bruno" };
+            for (int i = 0; i < 3; i++)
+            {
+                var account = new Account() { AccountNumber = i + 1, Balance = (i + 1) * 1000 };
+                var customer = new Customer() { CustomerAccounts = new List<Account>(), CustomerNumber = i + 1, Name = listOfRandomNames[i] };
+                customer.CustomerAccounts.Add(account);
+                listOfCustomers.Add(customer);
+            }
+            var acc = new Account() { AccountNumber = 100, Balance = 2222 };
+            listOfCustomers[1].CustomerAccounts.Add(acc);
+            this.CustomerList = listOfCustomers;
+        }
+    }
+}
--- CharlieBankApp/Startup.cs
+++ CharlieBankApp/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CharlieBankApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,7 +32,7 @@ namespace CharlieBankApp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddScoped<BankRepostitory>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
